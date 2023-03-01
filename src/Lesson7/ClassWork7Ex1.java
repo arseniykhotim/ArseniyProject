f9de5d98c596073c7ef6a2026457ac6b0c8c60eb
arseniykhotim <arseniy.khotim@gmail.com>
2023-03-01 19:27:51 +0300

    Git project

---
+++ src/Lesson7/ClassWork7Ex1.java
@@ -0,0 +1,8 @@
+package Less1.Lesson7;
+
+public class ClassWork7Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Minsk!");
+    }
+}
